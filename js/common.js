2628de93826f0a320c8ac3555c75aca6aa44a868
biubiu <3061317824@qq.com>
2024-06-06 22:15:53 +0800

    封装提示框函数

--- js/common.js
+++ js/common.js
@@ -1,2 +1,11 @@
 // 创建 axios 基地址
 axios.defaults.baseURL = 'https://hmajax.itheima.net'
+
+// 封装提示框函数
+const showToast = (msg) => {
+  const myToast = document.querySelector('.my-toast')
+  const toast = new bootstrap.Toast(myToast)
+  toast.show()
+  document.querySelector('.toast-body').innerHTML = msg
+}
+showToast('登录成功')
