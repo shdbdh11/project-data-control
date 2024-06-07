d6c7940172733444609e883fbcd06fbd5924f183
biubiu <3061317824@qq.com>
2024-06-07 22:06:06 +0800

    回显用户名、退出登录、添加响应拦截器

--- js/common.js
+++ js/common.js
@@ -9,12 +9,48 @@ const showToast = (msg) => {
   document.querySelector('.toast-body').innerHTML = msg
 }
 
+
+
+const data = localStorage.getItem('userMsg') ? JSON.parse(localStorage.getItem('userMsg')) : {}
 // token是否存在验证
 const checkToken = () => {
-  const data = localStorage.getItem('userMsg') ? JSON.parse(localStorage.getItem('userMsg')) : {}
   const { token } = data
   if (!token) {
     showToast('身份信息过期，请重新登录')
     setTimeout(e => { location.href = './login.html' }, 1500)
   }
 }
+
+// 根据本地存储回显用户名
+const renderUname = () => {
+  const { username } = data
+  if (username) {
+    document.querySelector('.username').innerHTML = username
+  }
+}
+
+// 退出登录
+const logout = () => {
+  document.querySelector('#logout').addEventListener('click', e => {
+    // 清空本地存储
+    localStorage.removeItem('userMsg')
+    showToast('退出登录成功')
+    setTimeout(() => {
+      location.href = './login.html'
+    }, 1500);
+  })
+}
+
+// 添加请求拦截器
+axios.interceptors.request.use(function (config) {
+  // 在发送请求之前做些什么
+  // console.log(config);
+  // 根据本地的 token 来发送请求  (但是没登录前是没有token的)
+  const { token } = data
+  if (token) config.headers['Authorization'] = token
+
+  return config;
+}, function (error) {
+  // 对请求错误做些什么
+  return Promise.reject(error);
+});
--- js/index.js
+++ js/index.js
@@ -2,3 +2,52 @@
  *  token验证
  */
 checkToken()
+
+/**
+ *  渲染用户名
+ */
+renderUname()
+
+/**
+ *  退出登录
+ */
+logout()
+
+/**
+ *  封装函数 => 获取数据
+ */
+const getData = async () => {
+  // const { token } = JSON.parse(localStorage.getItem('userMsg'))
+  try {
+    const res = await axios({
+      url: '/dashboard',
+      method: 'GET',
+      // headers: {
+      //   Authorization: token
+      // }
+    })
+    // console.log(res);
+    // 渲染数据
+    // 渲染overview
+    renderOverview(res.data.data.overview)
+  } catch (err) {
+    console.dir(err);
+    // 判断token是否过期，过期就提示用户并退出登录
+    if (err.response.status === 401) {
+      showToast('您的登录信息已过期，请重新登录')
+      localStorage.removeItem('userMsg')
+      setTimeout(() => {
+        location.href = './login.html'
+      }, 1500);
+    }
+  }
+}
+getData()
+// 渲染overview
+function renderOverview(overview) {
+  // console.log(overview);
+  // 渲染页面  overview的键 和 需要渲染的地方的类名一致
+  Object.keys(overview).forEach(item => {
+    document.querySelector(`.${item}`).innerHTML = overview[item]
+  });
+} 
