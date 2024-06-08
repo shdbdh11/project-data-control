6f49bb5049c0833d9d962cc1de364b5d285c1ec1
biubiu <3061317824@qq.com>
2024-06-08 22:31:19 +0800

    图表完成4/5

--- js/index.js
+++ js/index.js
@@ -18,11 +18,19 @@ logout()
  */
 const getData = async () => {
   const res = await axios.get('/dashboard')
+  console.log(res);
+  const { overview, year, salaryData, groupData } = res.data
   // 渲染数据
   // 渲染overview
-  renderOverview(res.data.overview)
+  renderOverview(overview)
   // 渲染  id="line"
-  renderLine(res.data.year)
+  renderYear(year)
+  // 渲染 salary
+  renderSalary(salaryData)
+  // 渲染 groupData
+  renderGroupData(groupData)
+  // 渲染 男女薪资分布
+  renderFnbu(salaryData)
 }
 getData()
 
@@ -34,13 +42,13 @@ function renderOverview(overview) {
   });
 }
 
-// 渲染 line   id="line"
-function renderLine(year) {
+// 渲染 year   id="line"
+function renderYear(year) {
   // console.log(year);
   // 基于准备好的dom，初始化echarts实例
   var myChart = echarts.init(document.querySelector('#line'));
   // 指定图表的配置项和数据
-  option = {
+  const option = {
     title: {
       text: '薪资走势',
       left: 10,
@@ -117,4 +125,209 @@ function renderLine(year) {
   };
   // 使用刚指定的配置项和数据显示图表。
   myChart.setOption(option);
+}
+
+// 渲染 salary
+function renderSalary(salaryData) {
+  // console.log(salaryData);
+  // 生成图标
+  // 初始化实例化对象
+  const myChart = echarts.init(document.querySelector('#salary'))
+  const option = {
+    title: {
+      text: '班级薪资分布',
+      top: 10,
+      left: 10,
+    },
+    tooltip: {
+      trigger: 'item'
+    },
+    legend: {
+      bottom: 5,
+      left: 'center'
+    },
+    series: [
+      {
+        name: '班级薪资分布',
+        type: 'pie',
+        // 圆饼大小 [圆饼内半径，圆饼外半径]
+        radius: ['55%', '80%'],
+        // 提示线堆叠策略
+        avoidLabelOverlap: false,
+        // 圆饼图连接处样式
+        itemStyle: {
+          borderRadius: 20,
+          borderColor: '#fff',
+          borderWidth: 5
+        },
+        label: {
+          show: false,
+          position: 'center'
+        },
+        emphasis: {
+          label: {
+            show: false,
+            // fontSize: 40,
+            // fontWeight: 'bold'
+          }
+        },
+        labelLine: {
+          show: false
+        },
+        data: salaryData.map(item => ({
+          value: item.g_count + item.b_count,
+          name: item.label
+        }))
+      }
+    ],
+    color: ['#fda224', '#5097ff', '#3abcfa', '#34d39a']
+
+  };
+  // 调用配置项
+  myChart.setOption(option)
+}
+
+// 渲染 班级每组薪资
+function renderGroupData(groupData) {
+  // console.log(groupData);
+  // console.log(groupData[1]);
+  // 初始化实例对象
+  const myChart = echarts.init(document.querySelector('#lines'))
+  // 配置项
+  const option = {
+    tooltip: {},
+    xAxis: {
+      type: 'category',
+      axisLine: {
+        lineStyle: {
+          type: 'dashed',
+          color: '#ccc'
+        }
+      },
+      axisLabel: {
+        color: '#999'
+      },
+      data: groupData[1].map(item => item.name)
+    },
+    yAxis: {
+      type: 'value',
+      splitLine: {
+        lineStyle: {
+          type: 'dashed',
+          color: '#ccc'
+        }
+      }
+    },
+    series: [
+      {
+        // data: [120, 200, 150, 80, 70, 110, 130],
+        data: groupData[1].map(item => item.hope_salary),
+        type: 'bar',
+        itemStyle: {
+          color: {
+            type: 'linear',
+            x: 0,
+            y: 0,
+            x2: 0,
+            y2: 1,
+            colorStops: [{
+              offset: 0, color: '#34D39A' // 0% 处的颜色
+            }, {
+              offset: 1, color: 'rgba(52,211,154,.2)' // 100% 处的颜色
+            }],
+            global: false // 缺省为 false
+          }
+        }
+      },
+      {
+        data: groupData[1].map(item => item.salary),
+        type: 'bar',
+        itemStyle: {
+          color: {
+            type: 'linear',
+            x: 0,
+            y: 0,
+            x2: 0,
+            y2: 1,
+            colorStops: [{
+              offset: 0, color: '#499fee' // 0% 处的颜色
+            }, {
+              offset: 1, color: 'rgba(73,159,238,.2)' // 100% 处的颜色
+            }],
+            global: false // 缺省为 false
+          }
+        }
+      }
+    ]
+  };
+  // 调用配置项
+  myChart.setOption(option)
+
+  // 切换数据  id="id="btns""
+  document.querySelector('#btns').addEventListener('click', e => {
+    if (e.target.tagName === 'BUTTON') {
+      document.querySelector('#btns').querySelector('.btn-blue').classList.remove('btn-blue')
+      e.target.classList.add('btn-blue')
+      const i = e.target.innerHTML
+      option.xAxis.data = groupData[i].map(item => item.name)
+      option.series[0].data = groupData[i].map(item => item.hope_salary)
+      option.series[1].data = groupData[i].map(item => item.salary)
+      // 重新渲染
+      myChart.setOption(option)
+    }
+  })
+}
+
+// 渲染 男女薪资分布
+function renderFnbu(fnbu) {
+  const myChart = echarts.init(document.querySelector('#gender'))
+  const option = {
+    title: [{
+      text: '男女薪资分布',
+      textStyle: { fontSize: 16 },
+      top: 10,
+      left: 7
+    },
+    {
+      text: '男生',
+      textStyle: { fontSize: 14 },
+      top: '45%',
+      left: '45%'
+    },
+    {
+      text: '女生',
+      textStyle: { fontSize: 14 },
+      top: '85%',
+      left: '45%'
+    }],
+    tooltip: {},
+    series: [
+      {
+        name: '男生',
+        type: 'pie',
+        radius: ['20%', '30%'],
+        center: ['50%', '30%'],
+        // roseType: 'area',//根据数据显示饼图大小
+        // itemStyle: {
+        //   borderRadius: 8
+        // },//边框圆角
+        // data: [
+        //   { value: 40, name: 'rose 1' },
+        //   { value: 33, name: 'rose 2' },
+        //   { value: 28, name: 'rose 3' },
+        //   { value: 22, name: 'rose 4' },
+        // ]
+        data: fnbu.map(item => ({ value: item.b_count, name: item.label }))
+      },
+      {
+        name: '女生',
+        type: 'pie',
+        radius: ['20%', '30%'],
+        center: ['50%', '70%'],
+        data: fnbu.map(item => ({ value: item.g_count, name: item.label }))
+      }
+    ],
+    color: ['#fda224', '#5097ff', '#3abcfa', '#34d39a']
+  };
+  myChart.setOption(option)
 }
